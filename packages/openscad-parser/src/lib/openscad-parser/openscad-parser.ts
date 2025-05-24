cde47838c03c2bcc6beed84b0d7ea278fe0dadaa
luciano <templar_3@hotmail.com>
2025-05-24 20:54:03 -0300

    feat: implement OpenSCAD parser with error handling and incremental parsing support

--- packages/openscad-parser/src/lib/openscad-parser/openscad-parser.ts
+++ packages/openscad-parser/src/lib/openscad-parser/openscad-parser.ts
@@ -49,7 +49,7 @@ import {
   Logger,
   // Assuming ErrorHandlerOptions and LoggerOptions might be exported from here or defined inline
   // For now, let's assume they are part of the options object structure directly
-} from '../handler/index';
+} from '@/lib';
 import { ChangeTracker } from './ast/changes/change-tracker'; // Change is not used
 
 /**
